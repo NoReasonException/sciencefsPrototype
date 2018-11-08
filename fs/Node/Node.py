81f42eb9b0f32afd2d100a603ce6ee0dbee49732
NoReasonException <stefannstefanou@gmail.com>
2018-11-08 05:46:27 +0000

    addons on Node

--- fs/Node/Node.py
+++ fs/Node/Node.py
@@ -15,26 +15,56 @@ class Node:
 
     """
     @staticmethod
-    def create(dentry,fs,mode,typeoffile):
+    def create(fs,parent,name,mode,typeoffile):
         now=time()
-        return Inode(dentry,now,now,now,2,fs,mode,typeoffile)
+        return Node(parent,name,now,now,now,2,fs,mode,typeoffile)
     @staticmethod
-    def createRegularFile(dentry,fs,mode):
-        return Inode.create(dentry,fs,mode,S_IFREG)
+    def createRegularFile(fs,parent,name,mode):
+        return Node.create(fs,parent,name,mode,S_IFREG)
     @staticmethod
-    def createDirectoryFile(dentry,fs,mode):
-        return Inode.create(dentry,fs,mode,S_IFDIR)
-    def __init__(self,dentry,atime,mtime,ctime,nlink,fs,mode,typeoffile):
-        self.__dentry=dentry
+    def createDirectoryFile(fs,parent,name,mode):
+        return Node.create(fs,parent,name,mode,S_IFDIR)
+    def __init__(self,parent,name,atime,mtime,ctime,nlink,fs,mode,typeoffile):
+        self.__parent=parent
+        self.__name=name
         self.__size=0
-        self.__data=None
         self.__atime=atime
         self.__mtime=mtime
         self.__ctime=ctime
         self.__nlink=nlink
         self.__fs=fs
         self.__mode=(mode|typeoffile)
-    def getDentry   (self):return self.__dentry
+        if(typeoffile==S_IFDIR):
+            self.__data=list()
+        else:
+            self.__data=None
+    @staticmethod
+    def pathToNodeTranslator(rootNode,path):
+        tmp=rootnode
+        pathelements=path.split('/')
+        
+            
+    
+    def getFullPath (self):
+        retval=[self.getName()]
+        tmp=self
+        
+        while(not tmp.isRoot()):
+            tmp=tmp.getParent()
+            retval.append(tmp.getName())
+            
+        """
+        reverse and convert to standard path
+        """
+        return str(retval[::-1]).\
+                        replace("[","").\
+                        replace("]","").\
+                        replace(",","").\
+                        replace("'","").\
+                        replace(" ","/")[1:]
+
+    def getName     (self):return self.__name
+    def getParent   (self):return self.__parent
     def getSize     (self):return self.__size
     def getATime    (self):return self.__atime
     def getMTime    (self):return self.__mtime
@@ -44,6 +74,18 @@ class Node:
     def getMode     (self):return self.__mode
     def isFile      (self):return (self.__mode&S_IFREG)!=0
     def isDirectory (self):return (self.__mode&S_IFDIR)!=0
-    def getData     (self):return self__data
-    
+    def getData     (self):return self.__data
+    def isRoot      (self):return self.__parent==None
+
+    def setData     (self,data):self.__data=data
+    def getChildren (self):
+        if self.isFile():
+            raise Exception('Regular files cant have children')
+        return tuple(self.getData())
+    def addChildren(self,child):
+        if self.isFile():
+            raise Exception('Regular files cant have children')
+        self.__data.append(child)
+        child.__parent=self
+        return child
 
