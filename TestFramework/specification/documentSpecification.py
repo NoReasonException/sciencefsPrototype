6497ff6d5b4101389f59f196d3529e8de597425e
noReasonException <stefannstefanou@gmail.com>
2019-01-28 00:19:06 +0000

    More utills for test framework(doc on the way)

--- TestFramework/specification/documentSpecification.py
+++ TestFramework/specification/documentSpecification.py
@@ -1,20 +1,47 @@
 import json
 import time
-import datetime
-
+from datetime import datetime
+from numpy.random import normal
+from scipy.stats import norm
 class DateRangeBuilder:
     @staticmethod
-    def build(minUnixDate:int,maxUnixDate:int):
+    def simpleBuilder(numberOfObjects:int,minUnixDate:int,maxUnixDate:int):
         """
-        .build
+        .simpleBuilder
 
-        @params minUnixDate -> the minimum date of the sample
-        @params maxUnixDate -> the maximum date of the sample
-
-        This method returns a array of dates , following a 
-        standarized normal ((0,1)) distrubution with min -> minUnixDate and 
+        @params minUnixDate     -> the minimum date of the sample
+        @params maxUnixDate     -> the maximum date of the sample
+        @params numberOfFiles   -> the number of files returned
+        This method returns an array of iso8601 strings , following a  Normal
+        Distribution of N(m=maxUnixDate/minUnixDate),s=#one month from mean(2592000))
         
-        max-> maxUnixDate. 
         """
-        pass
+        m=(minUnixDate+maxUnixDate)/2
+        s=2592000
+        return DateRangeBuilder._build_raw(numberOfObjects,m,s)
+
+    @staticmethod
+    def _build_raw(numberOfObjects:int,mean:int,stddev:int):
+        retval=list()
+        for i in range(numberOfObjects):
+            retval.append(  datetime.utcfromtimestamp(normal(mean,stddev)).strftime('%Y-%m-%d %H:%M:%S'))
+        return retval
     
+        
+class DateRangeRetriever:
+    @staticmethod
+    def getMaxOfGivenPercent(minUnixDate:int,maxUnixDate:int,percent:int)->int:
+        assert percent>0 and percent<1
+        m=(minUnixDate+maxUnixDate)/2
+        s=2592000
+        return norm.ppf(percent,m,s)
+
+class Utills:
+    
+    @staticmethod
+    def _to8601(unixTimestamp:int)->str:
+        return datetime.utcfromtimestamp(unixTimestamp).strftime('%Y-%m-%d %H:%M:%S')
+    
+    @staticmethod
+    def _to8601_array(unixTimestampArray:list)->list:
+        return filter(lambda x :Utills._to8601(x),unixTimestampArray)
